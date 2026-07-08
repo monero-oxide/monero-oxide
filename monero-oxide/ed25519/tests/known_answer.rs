143437f87e1d064771f92e9752ede46dd2f3bb0e
j-berman <justinberman@protonmail.com>
2026-07-08 10:19:51 -0700

    Use Blake2bMonero for unbiased hash to point

--- monero-oxide/ed25519/tests/known_answer.rs
+++ monero-oxide/ed25519/tests/known_answer.rs
@@ -1,11 +1,11 @@
 #![allow(missing_docs)]
 
-use monero_ed25519::Point;
+use monero_ed25519::{CompressedPoint, Point};
 
 mod hex;
 
 #[test]
-fn hash_to_curve() {
+fn known_answer() {
   let reader = include_str!("./tests.txt");
 
   for line in reader.lines() {
@@ -13,7 +13,17 @@ fn hash_to_curve() {
 
     let command = words.next().unwrap();
     match command {
-      "check_key" => {}
+      "check_key" => {
+        let key = hex::decode(words.next().unwrap());
+        let expected = match words.next().unwrap() {
+          "true" => true,
+          "false" => false,
+          _ => unreachable!("invalid result"),
+        };
+
+        let actual = CompressedPoint::from(key).decompress();
+        assert_eq!(actual.is_some(), expected);
+      }
       "biased_hash_to_ec" => {
         let preimage = hex::decode(words.next().unwrap());
         let actual = Point::biased_hash(preimage);
@@ -21,7 +31,8 @@ fn hash_to_curve() {
         assert_eq!(actual.compress().to_bytes(), expected);
       }
       "derive_key_image_generator" => {
-        let point = hex::decode(words.next().unwrap());
+        let word = words.next().unwrap();
+        let point = hex::decode(word);
         let biased: bool = words.next().unwrap().parse().unwrap();
         let actual = if biased { Point::biased_hash(point) } else { Point::hash(point) };
         let expected = hex::decode(words.next().unwrap());
